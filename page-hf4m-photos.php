fbf3bb78432ec2902929b0bf46a7ca29b1f8ab8f
Kieran Reynolds <kreynolds@one-k.com>
2015-10-24 10:42:41 -0700

    Updated styles for hr and lightbox

--- page-hf4m-photos.php
+++ page-hf4m-photos.php
@@ -4,5 +4,6 @@ get_header(); ?>
 	<div class="page-photos full-width">
 		<h1><?php the_title();?></h1>
 		<hr />
+		<?php echo do_shortcode("[huge_it_gallery id='2']"); ?>
 	</div>
 <?php get_footer(); ?>
