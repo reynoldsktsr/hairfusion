fe4415cdde1a4a83d4a3b6f1348c6dfee5fd0d9a
Kieran Reynolds <kreynolds@one-k.com>
2015-11-01 23:26:15 -0800

    Added temp content to home boxes

--- page-hf4m-home.php
+++ page-hf4m-home.php
@@ -2,49 +2,71 @@
 // Template Name: Hf4M Home
 get_header(); ?>
 	<div class="page-home full-width">
-		<?php echo do_shortcode('[fusionslider name="homeslider"][/fusionslider]');?>
 		<div class="row">
-			<div class="col-md-4 homebox">
-				<div>Gallery</div>
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<?php echo do_shortcode('[fusionslider name="homeslider"][/fusionslider]');?>
 			</div>
-			<div class="col-md-4 homebox">
-				<div>Videos</div>
-			</div>
-			<div class="col-md-4 homebox">
-				<div>Subscribe</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row"></div>
+		<div class="col-sm-4 col-xs-6 homebox">
+			<div><?php echo do_shortcode('[fusionslider name="menofhairfusion"][/fusionslider]');?></div>
+		</div>
+		<div class="col-sm-4 col-xs-6 homebox">
+			<div>
+				<?php while ( have_posts() ) : the_post();
+				echo "<iframe id='homepagevideo' width='100%' height='100%' src='" . get_the_content() . "?rel=0&amp;showinfo=0;autoplay=1' frameborder='0'></iframe>";
+				endwhile; ?>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-4 homebox">
-				<div>Process</div>
+		<div class="col-sm-4 col-xs-6 homebox">
+			<div>
+				<h2>Subscribe</h2>
+				<hr>
+				<p>You should subscribe to recieve more information and special offers. This is temporary content and can be replaced.</p>
+				<button id="redirectSubscribe">Subscribe</button>
 			</div>
-			<div class="col-md-4 homebox">
-				<div>Contact Us</div>
+		</div>
+		<div class="col-sm-4 col-xs-6 homebox">
+			<div>
+				<h2>Process</h2>
+				<hr />
+				<p>Check out the process that we take. Yada yada words need to be put here to give the viewers a bit of an idea on how hair fusion works.</p>
+				<button>Read More</button>
 			</div>
-			<div class="col-md-4 homebox">
-				<div>Blog
-				<?php
-				$args = array(
-					'cat' => 4,
-					'posts_per_page' => 1
-					);
-				query_posts( $args );
-				while (have_posts()) : the_post();
-				$content = substr(get_the_content(),0,200);
-				echo "<p>" . $content . "...</p>";
-				echo "<a href='".get_permalink()."'>Read More</a>";
-				// echo get_permalink();
-				endwhile;
-				?>
-				</div>
+		</div>
+		<div class="col-sm-4 col-xs-6 homebox">
+			<div>
+				<h2>Contact Us</h2>
+				<hr>
+				<img src="/wp-content/uploads/2015/10/hf4m_logo_64.png">
+				<p>Replace image above with whatever, and for more information click below to contact us.</p>
+				<button>Contact Us</button>
+			</div>
+		</div>
+		<div class="col-sm-4 col-xs-6 homebox">
+			<div>
+				<h2>Blog</h2>
+				<hr>
+			<?php
+			$args = array(
+				'cat' => 4,
+				'posts_per_page' => 1
+				);
+			query_posts( $args );
+			while (have_posts()) : the_post();
+			$content = substr(get_the_content(),0,200);
+			echo "<p>" . $content . "...</p>";
+			echo "<a href='".get_permalink()."'>Read More</a>";
+			endwhile;
+			?>
 			</div>
 		</div>
 	</div>
 	<script type="text/javascript">
-		$( document ).ready(function() {
-			var width = $(".homebox").width();
-
-		    $(".homebox").height(width/2);
-		});
+	document.getElementById('redirectSubscribe').onclick = function() {
+		location.href = "/subscribe";
+	}
 	</script>
 <?php get_footer(); ?>
---
+++ page-hf4m-subscribe.php
@@ -0,0 +1,6 @@
+<?php
+// Template Name: Hf4M Subscribe
+get_header(); ?>
+<div class="page-subscribe full-width">
+</div>
+<?php get_footer();?>
