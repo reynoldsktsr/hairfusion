f89f06b7caa9702b23f22b82724c2840e95d953a
Kieran Reynolds <kreynolds@one-k.com>
2015-11-16 11:23:14 -0800

    Add menus to some pages

--- page-hf4m-about-us.php
+++ page-hf4m-about-us.php
@@ -1,14 +1,23 @@
 <?php
 // Template Name: Hf4M About Us
-get_header(); ?>
+get_header(); 
+while(have_posts()): the_post();
+?>
 	<div class="page-aboutus full-width container">
-		<h1><?php the_title();?></h1>
-		<hr />
-		<p>We are the leader in Scalp Micro Pigmentation. In this new industry that offers a different, better solution to the problems of hair loss, we have the most experience and lead the field in innovations that directly lead to the best results and your satisfaction.</p>
-		<p>As the original creators of CTHR™, and with unsurpassed experience, GLI has been on the forefront of perfecting and advancing Scalp Micro Pigmentation. Headquartered in Minneapolis, Minnesota our facility boasts 6,000 square feet of cutting edge technology and highly-trained staff to provide you with unsurpassed results.</p>
-		<p>We know the frustration that our clients experience with finding the right solution for their hair loss. We know because we’ve tried them all. That’s the reason we started Good Look Ink in the first place. Evaluating other "solutions" such as hair pieces, surgery, plugs and sprays are all part of your decision making process. We have insights into all of these and are here to help you with your decision. Our aim is to provide our clients a future in which they never have to think about their hair loss again.</p>
-		<p>Hair Fusion 4 Men is a new and innovative technique created specifically to address the hair loss needs of men of color. For years, the 3.5 billion dollar hair loss industry has been unable to provide a viable hair loss solution that men of color find acceptable, which has led us in many cases to shave our heads bald or settle for solutions that will provide less than desired results.</p>
-		<p>While there is nothing wrong with a bald head, for those that have the appropriate shaped head and can get a shave that will eliminate the shadow that reveals your receding hairline or bald spot </p>
+		<div class="col-sm-3">
+			<?php 
+			$defaults = array(
+				'menu' => 'About',
+				'menu_class' => 'about-menu'
+				);
+			wp_nav_menu($defaults); 
+			?>
+		</div>
+		<div class="col-sm-9">
+			<h1><?php the_title();?></h1>
+			<hr />
+			<?php the_content();?>
+		</div>
 
 	</div>
-<?php get_footer(); ?>
+<?php endwhile; get_footer(); ?>
--- page-hf4m-process.php
+++ page-hf4m-process.php
@@ -1,8 +1,22 @@
 <?php
 // Template Name: Hf4M Process
-get_header(); ?>
+get_header();
+while(have_posts()): the_post();
+?>
 	<div class="page-process full-width container">
-		<h1><?php the_title();?></h1>
-		<hr />
+		<div class="col-sm-3">
+			<?php 
+			$defaults = array(
+				'menu' => 'FAQ',
+				'menu_class' => 'faq-menu'
+				);
+			wp_nav_menu($defaults); 
+			?>
+		</div>
+		<div class="col-sm-9">
+			<h1><?php the_title();?></h1>
+			<hr />
+			<?php the_content();?>
+		</div>
 	</div>
-<?php get_footer(); ?>
+<?php endwhile; get_footer(); ?>
