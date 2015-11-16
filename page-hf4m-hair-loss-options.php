3173f95786976f36910f1a8c5fe5bd7ea28777bd
Kieran Reynolds <kreynolds@one-k.com>
2015-11-16 11:30:10 -0800

    Add another menu

--- page-hf4m-hair-loss-options.php
+++ page-hf4m-hair-loss-options.php
@@ -3,9 +3,20 @@
 get_header(); ?>
 	<div class="page-aboutus full-width container">
 	<?php while (have_posts()): the_post();?>
-		<h1><?php the_title();?></h1>
-		<hr />
-		<?php echo the_content();?>
+		<div class="col-sm-3">
+			<?php 
+			$defaults = array(
+				'menu' => 'MPB',
+				'menu_class' => 'mpb-menu'
+				);
+			wp_nav_menu($defaults); 
+			?>
+		</div>
+		<div class="col-sm-9">
+			<h1><?php the_title();?></h1>
+			<hr />
+			<?php the_content();?>
+		</div>
 	<?php endwhile;?>
 	</div>
 <?php get_footer(); ?>
